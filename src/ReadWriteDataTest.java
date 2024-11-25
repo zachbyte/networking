bf7c6b8a1ae661eb2138ab62a695b35c59fd47bf
zachbyte <zach@zachbyte.com>
2024-11-25 10:31:35 -0700

    removed examples

--- src/ReadWriteDataTest.java
+++
@@ -1,9 +0,0 @@
-// Create and start a ReadWriteData object.
-import javax.swing.JFrame;
-
-public class ReadWriteDataTest {
-  public static void main(String[] args) {
-    ReadWriteData application = new ReadWriteData();
-    application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-}
